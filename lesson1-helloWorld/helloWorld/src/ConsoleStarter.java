6096c7059874cbaf3878dc19115ccaeeb1241183
susfenix60 <fenix7096yt@gmail.com>
2024-10-01 20:22:29 +0200

    Implement simple calculator in console

---
+++ lesson1-helloWorld/helloWorld/src/ConsoleStarter.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class ConsoleStarter {
+    public static void main(String[] args) {
+        startCalculator();
+    }
+
+    public static void startCalculator() {
+
+        // Skaner to przyjmowanie informacji od użytkownika \\
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println("Calculator has started");
+
+        int chosenOption = -1;
+        while (chosenOption != 0) {  //while - dopóki warunek jest prawdziwy/zgadza się\\
+
+            showMenu();
+            chosenOption = scanner.nextInt();
+            if (chosenOption == 1) {
+                doAddOperation(scanner);
+
+            }
+        }
+    }
+
+
+    private static void showMenu() {
+        System.out.println(
+                """
+                        Witaj w aplikacji KALKULATOR
+                        Wybierz co chcesz zrobić:
+                        1.Dodaj liczby
+                        2.Odejmij liczby
+                        3.Podziel liczby
+                        4.Pomnóż liczby
+                        5.Potęguj liczby
+                        6.Wykonaj dzielenie z resztą
+                        0.Wyjście
+                                      
+                        WYBIERZ OPCJE:
+                        """
+        );
+    }
+
+    private static void doAddOperation(Scanner scanner) {
+
+        System.out.print("Podaj pierwszą liczbe: ");
+        double firstUserNumber = scanner.nextDouble();
+
+        System.out.print("Podaj drugą liczbe: ");
+        double secondUserNumber = scanner.nextDouble();
+
+        double added = SimpleCalculator.add(firstUserNumber, secondUserNumber);
+
+        System.out.println("Wynik to: " + added);
+
+    }
+
+}
