2f72e18e5271ed44f15c5cbfb3ac7e80a77bdebe
susfenix60 <fenix7096yt@gmail.com>
2024-10-15 19:09:34 +0200

    Add second way to checking the palindrom

--- lesson1-helloWorld/helloWorld/src/lesson2/PalindromResolver.java
+++ lesson1-helloWorld/helloWorld/src/lesson2/PalindromResolver.java
@@ -32,12 +32,35 @@ public class PalindromResolver {
         return true;
     }
 
+    public static boolean isPalindrom2(String word) {
+
+        String reversedWord = reverse(word);
+        return word.equalsIgnoreCase(reversedWord);
+
+    }
+
+    private static String reverse(String word) {
+
+        char[] wordInCharArray = word.toCharArray();
+        String reversedWord = "";
+        for (int currentCharIndex = wordInCharArray.length - 1; currentCharIndex >= 0; currentCharIndex = currentCharIndex - 1) {
+            char currentCharacter = wordInCharArray[currentCharIndex];
+            reversedWord += currentCharacter;
+        }
+        return reversedWord;
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("isPalindrom('Kajak')= " + isPalindrom("Kajak"));
-        System.out.println("isPalindrom('ala')= " + isPalindrom("ala"));
-        System.out.println("isPalindrom('oko')= " + isPalindrom("oko"));
-        System.out.println("isPalindrom('Michal')= " + isPalindrom("Michal"));
+        System.out.println("isPalindrom('Kajak')= " + reverse("Kajak"));
+        System.out.println("isPalindrom('ala')= " + reverse("ala"));
+        System.out.println("isPalindrom('oko')= " + reverse("oko"));
+        System.out.println("isPalindrom('Michal')= " + reverse("Michal"));
+        System.out.println(" \n");
+        System.out.println("isPalindrom('Kajak')= " + isPalindrom2("Kajak"));
+        System.out.println("isPalindrom('ala')= " + isPalindrom2("ala"));
+        System.out.println("isPalindrom('oko')= " + isPalindrom2("oko"));
+        System.out.println("isPalindrom('Michal')= " + isPalindrom2("Michal"));
 
 
     }
