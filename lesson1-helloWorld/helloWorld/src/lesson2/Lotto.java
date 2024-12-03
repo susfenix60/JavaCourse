1b90f28c25744f5f28775d57acee3600cb10b93e
susfenix60 <fenix7096yt@gmail.com>
2024-12-03 20:06:17 +0100

    Ending implement Lotto game

--- lesson1-helloWorld/helloWorld/src/lesson2/Lotto.java
+++ lesson1-helloWorld/helloWorld/src/lesson2/Lotto.java
@@ -1,9 +1,6 @@
 package lesson2;
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Lotto {
 
@@ -18,17 +15,23 @@ public class Lotto {
 
         Set<Integer> userNumbers = retrieveUserNumbers();
         Set<Integer> computerNumbers = retrieveComputerNumbers();
+        System.out.println("User numbers: " + userNumbers);
+
         System.out.println("COMPUTER NUMBERS: " + computerNumbers);
-        // 3. countHits();
-        // 4. determineWinMoney();
-        // 5. calculateStatistics();
+        int hitsCounter = countHits(computerNumbers, userNumbers);
+        System.out.println("Right guessed hits: " + hitsCounter);
+        double wonMoney = determineWinMoney2(hitsCounter);
+        System.out.println("Won money: " + wonMoney);
+        int winRateThree = calculateStatistics(userNumbers, 3);
+        int winRateFour = calculateStatistics(userNumbers, 4);
+        System.out.println("winRateThree: " + winRateThree);
+        System.out.println("winRateFour: " + winRateFour);
         System.out.println("The Lotto game has ended");
     }
 
-
     private static Set<Integer> retrieveUserNumbers() {
 
-        Set<Integer> userNumbers = new HashSet<>();
+        Set<Integer> userNumbers = new TreeSet<>();
         Scanner scanner = new Scanner(System.in);
 
         while (userNumbers.size() < 6) {
@@ -49,14 +52,92 @@ public class Lotto {
 
     private static Set<Integer> retrieveComputerNumbers() {
         Random random = new Random();
-        Set<Integer> computerNumbers = new HashSet<>();
+        Set<Integer> computerNumbers = new TreeSet<>();
         while (computerNumbers.size() < 6) {
             int currentRandomNumber = random.nextInt(49) + 1;
             computerNumbers.add(currentRandomNumber);
         }
 
+
         return computerNumbers;
     }
 
+    private static int countHits(Set<Integer> computerNumbers, Set<Integer> userNumbers) {
 
+        int rightHits = 0;
+
+        for (int currentHitCheck : userNumbers) {
+            for (int currentComputerNumber : computerNumbers) {
+                if (currentComputerNumber == currentHitCheck) {
+                    rightHits++;
+                }
+            }
+        }
+
+        return rightHits;
+    }
+
+    private static int countHits2(Set<Integer> computerNumbers, Set<Integer> userNumbers) {
+
+        int rightHits = 0;
+
+        for (int currentHitCheck : userNumbers) {
+            if (computerNumbers.contains(currentHitCheck)) {
+                rightHits++;
+            }
+        }
+
+        return rightHits;
+    }
+
+    //W Lotto Plus każda wygrana ma stałą wysokość. Trafienie 6 z 49 liczb oznacza wygraną 1 000 000 zł. „Piątka” to 3500 zł, „czwórka” – 100 zł, a „trójka” – 10 zł.
+    private static double determineWinMoney(int hitsCounter) {
+
+        double win = 0;
+
+        if (hitsCounter == 6) {
+            win = 1_000_000;
+        } else if (hitsCounter == 5) {
+            win = 3500;
+        } else if (hitsCounter == 4) {
+            win = 100;
+        } else if (hitsCounter == 3) {
+            win = 10;
+        }
+
+        return win;
+    }
+
+    private static double determineWinMoney2(int hitsCounter) {
+        switch (hitsCounter) {
+            case 6:
+                return 1_000_000;
+            case 5:
+                return 3500;
+            case 4:
+                return 100;
+            case 3:
+                return 10;
+
+            default:
+                return 0;
+        }
+    }
+
+    private static int calculateStatistics(Set<Integer> userNumbers, int hitRate) {
+        int winRateCounter = 0;
+        int randomRate = 0;
+
+        while (winRateCounter < hitRate) {
+            randomRate++;
+            Set<Integer> computerNumbers = retrieveComputerNumbers();
+            int hitCounter = countHits(computerNumbers, userNumbers);
+
+            if (hitCounter >= hitRate) {
+                winRateCounter++;
+            }
+
+        }
+        return randomRate;
+    }
 }
