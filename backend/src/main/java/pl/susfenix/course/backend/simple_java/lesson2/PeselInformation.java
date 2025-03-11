5b4ece1efbb0e54f06a816db858694afe49dc436
susfenix60 <fenix7096yt@gmail.com>
2025-03-11 20:30:36 +0100

    implement gender extracter, and birth date extracter

--- backend/src/main/java/pl/susfenix/course/backend/simple_java/lesson2/PeselInformation.java
+++ backend/src/main/java/pl/susfenix/course/backend/simple_java/lesson2/PeselInformation.java
@@ -1,7 +1,5 @@
 package pl.susfenix.course.backend.simple_java.lesson2;
 
-import java.time.LocalDate;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class PeselInformation {
@@ -9,13 +7,112 @@ public class PeselInformation {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj Pesel");
+        System.out.println("Please enter your pesel.");
         String userInput = scanner.next();
 
         boolean isValid = isValid(userInput);
-        System.out.println(isValid);
+        System.out.println("Your pesel is " + isValid);
+        System.out.println("You are a " + extractGender(userInput));
+        System.out.println("You were born in " + extractBirthMonth(userInput));
 
         //System.out.println(LocalDate.now());
+        scanner.close();
+    }
+
+    private static String extractGender(String pesel) {
+        char genderDigitAsChar = pesel.charAt(9);
+        int genderDigit = Character.getNumericValue(genderDigitAsChar);
+        if (genderDigit % 2 == 1) {
+            return "male";
+        } else {
+            return "female";
+        }
+    }
+
+    private static String extractBirthDate(String pesel) {
+
+        int fullYear = extractFullYear(pesel);
+        int month = extractNumericPeselMonth(pesel) %20;
+
+        return null;
+
+    }
+
+    private static int extractFullYear(String pesel) {
+        int century = extractCentury(pesel);
+
+        char birthYearAsChar1 = pesel.charAt(0);
+        char birthYearAsChar2 = pesel.charAt(1);
+        int birthYear1 = Character.getNumericValue((birthYearAsChar1));
+        int birthYear2 = Character.getNumericValue((birthYearAsChar2));
+        int birthYear = century + (birthYear1 * 10 + birthYear2);
+
+        return birthYear;
+    }
+
+    //TODO implement extractBirthDay(..)
+
+    private static int extractCentury(String pesel) {
+        int birthMonth = extractNumericPeselMonth(pesel);
+
+        int century = -1;
+
+        if (birthMonth > 80) {
+            century = 1800;
+        } else if (birthMonth > 60 && birthMonth < 80) {
+            century = 2200;
+        } else if (birthMonth > 40 && birthMonth < 60) {
+            century = 2100;
+        } else if (birthMonth > 20 && birthMonth < 40) {
+            century = 2000;
+        } else {
+            century = 1900;
+        }
+        return century;
+    }
+
+    private static int extractNumericPeselMonth(String pesel) {
+        char birthMonthAsChar1 = pesel.charAt(2);
+        char birthMonthAsChar2 = pesel.charAt(3);
+        int birthMonth1 = Character.getNumericValue((birthMonthAsChar1));
+        int birthMonth2 = Character.getNumericValue((birthMonthAsChar2));
+        int birthMonth = birthMonth1 * 10 + birthMonth2;
+        return birthMonth;
+    }
+
+    private static String extractBirthMonth(String pesel) {
+
+        int birthMonth = extractNumericPeselMonth(pesel) %20;
+        switch (birthMonth) {
+            case 1:
+                return "January";
+            case 2:
+                return "February";
+            case 3:
+                return "March";
+            case 4:
+                return "April";
+            case 5:
+                return "May";
+            case 6:
+                return "June";
+            case 7:
+                return "July";
+            case 8:
+                return "August";
+            case 9:
+                return "September";
+            case 10:
+                return "October";
+            case 11:
+                return "November";
+            case 12:
+                return "December";
+            default:
+                throw new IllegalStateException("Should never happen");
+
+        }
+
     }
 
     private static boolean isValid(String userInput) {
